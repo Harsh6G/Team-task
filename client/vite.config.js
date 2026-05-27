09fa7da7fc1e3888da64acde9ed47c411baf1ad9
Harsh Dixit <harsh.dixit2024e@vitstudent.ac.in>
2026-05-27 13:03:41 +0530

    Add preview configuration to Vite setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    allowedHosts: true
+  }
 });
